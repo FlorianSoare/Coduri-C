a6efa8c1dd5a3222b8b979ee320077ff0fb8a258
Florian Soare <florian.soare@takmate.com>
2018-02-01 18:48:41 +0200

    Laborator SD

--- SD/L1/LASD1.c
+++ SD/L1/LASD1.c
@@ -23,12 +23,23 @@ Astfel valorile vor diferi la fiecare rulare a programului */
 
 randomseed() {
 
-    srand(time(NULL));
+    srand(time(NULL)); /*srand este seed-rand care executa aleator in functie de timp*/ 
 
     printf("%d %d %d\n",rand(),rand(),rand());
 
 return 0 ;
 
+}
+
+/* o functie ce returneaza un numar aleator de la 1 la o valoare n ceruta de aplicatie */
+random1n(){
+	int n;
+	printf("Introdu o valoare: ");
+	scanf("%d", &n);
+	printf ("O valoare aleatoare de la 1 la %d este %d",n,rand()%n+1);
+	
+	return 0;
+	
 }
 
 
@@ -37,5 +48,5 @@ return 0 ;
 int main(){
 	random();
 	randomseed();
-	
+	random1n();
 }
